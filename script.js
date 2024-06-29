1462c5e19269a1251129a66db659f94c363d0274
code.lucho33 <code.lucho33@gmail.com>
2024-06-29 06:26:41 -0500

    crear arreglos

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+/**
+ * La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+ * 
+ * 
+ * 
+ * 
+ */
 
+let caracteresBuscados= ['e', 'i', 'a' , 'o' ,'u'];
+let caracteresClave= ['enter','imes','ai','ober','ufat'];
+let arregloDeCaracters = [];
+let texto;
 
 function getText(){
-let texto = document.getElementById('texto__encriptable').value;
-alert(texto);
+texto=document.getElementById('texto__encriptable').value;
+
+console.log(texto);
+
+
 
 }
 
