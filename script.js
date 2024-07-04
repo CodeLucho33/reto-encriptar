5e197f15c0028cbe626b363fd45757e5f0b2dee6
CodeLucho33 <code.lucho33@gmail.com>
2024-07-04 07:07:35 -0500

    Implementar llamado de función para enciptar y desencriptar

--- script.js
+++ script.js
@@ -23,8 +23,22 @@ let textoEncriptado = [];
 
 function getText() {
     texto = document.getElementById('texto__encriptable').value;
-   
+   texto = texto.replaceAll('e','enter');
+   texto =texto.replaceAll('i','imes') ;
+   texto =texto.replaceAll('a','ai') ;
+   texto =texto.replaceAll('o','ober') ;
+   texto =texto.replaceAll('i','ufat') ;
+console.log(texto);
 
 
 }
 
+function reverseText(){
+    let desencriptado = document.getElementById('texto__encriptable').value;
+    desencriptado = desencriptado.replaceAll('enter','e');
+    desencriptado = desencriptado.replaceAll('imes','i');
+    desencriptado = desencriptado.replaceAll('ai','a');
+    desencriptado = desencriptado.replaceAll('ober','o');
+    desencriptado = desencriptado.replaceAll('ufat','i');
+console.log(desencriptado);
+}
