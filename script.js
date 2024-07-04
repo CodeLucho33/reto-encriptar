8572fe5852dd1f38bed270fb3d0215f9689d16de
CodeLucho33 <code.lucho33@gmail.com>
2024-07-04 06:22:30 -0500

    Borrar intento par arreglos

--- script.js
+++ script.js
@@ -23,23 +23,8 @@ let textoEncriptado = [];
 
 function getText() {
     texto = document.getElementById('texto__encriptable').value;
-    //Lllamo al método Split de los strings para que me divida la cadena de caracteres
-    //Y almaceno los caracteres en un arreglo
-    arregloDeCaracters = texto.split('');
+   
 
-    // Recorrer el arreglo y remplazar claves
-    for (let i = 0; arregloDeCaracters.length; i++) {
-        if (arregloDeCaracters[i] === 'e') {
-            textoEncriptado.push(keyE);
-        } else {
-            textoEncriptado.push(arregloDeCaracters[i]);
-            break;
-        }
-       
-    }
-let mensaje = textoEncriptado.join('');
-console.log(`Texto original ${texto}`);
-console.log(`Texto encriptado ${mensaje}`);
 
 }
 
