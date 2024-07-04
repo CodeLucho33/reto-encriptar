1be90d21665889677b71c717edffe723f3cd4d78
CodeLucho33 <code.lucho33@gmail.com>
2024-07-04 05:21:56 -0500

    arreglos

--- script.js
+++ script.js
@@ -9,18 +9,37 @@ La letra "u" es convertida para "ufat"
  * 
  * 
  */
+/**  let caracteresBuscados= ['e', 'i', 'a' , 'o' ,'u'];
+let caracteresClave= ['enter','imes','ai','ober','ufat'];  */
 
-let caracteresBuscados= ['e', 'i', 'a' , 'o' ,'u'];
-let caracteresClave= ['enter','imes','ai','ober','ufat'];
 let arregloDeCaracters = [];
-let texto;
+let texto = "prueba";
+let keyE = 'enter';
+let keyI = 'imes';
+let keyA = 'ai';
+let keyO = 'ober';
+let keyU = 'ufat';
+let textoEncriptado = [];
 
-function getText(){
-texto=document.getElementById('texto__encriptable').value;
+function getText() {
+    texto = document.getElementById('texto__encriptable').value;
+    //Lllamo al método Split de los strings para que me divida la cadena de caracteres
+    //Y almaceno los caracteres en un arreglo
+    arregloDeCaracters = texto.split('');
 
-console.log(texto);
-
-
+    // Recorrer el arreglo y remplazar claves
+    for (let i = 0; arregloDeCaracters.length; i++) {
+        if (arregloDeCaracters[i] === 'e') {
+            textoEncriptado.push(keyE);
+        } else {
+            textoEncriptado.push(arregloDeCaracters[i]);
+            break;
+        }
+       
+    }
+let mensaje = textoEncriptado.join('');
+console.log(`Texto original ${texto}`);
+console.log(`Texto encriptado ${mensaje}`);
 
 }
 
