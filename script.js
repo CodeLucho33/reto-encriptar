ab741a81a01be4fa64bba0d0bf3cdbf6ef8fc9a1
CodeLucho33 <code.lucho33@gmail.com>
2024-07-04 08:42:24 -0500

    Implementar mensaje en pantalla

--- script.js
+++ script.js
@@ -12,14 +12,7 @@ La letra "u" es convertida para "ufat"
 /**  let caracteresBuscados= ['e', 'i', 'a' , 'o' ,'u'];
 let caracteresClave= ['enter','imes','ai','ober','ufat'];  */
 
-let arregloDeCaracters = [];
-let texto = "prueba";
-let keyE = 'enter';
-let keyI = 'imes';
-let keyA = 'ai';
-let keyO = 'ober';
-let keyU = 'ufat';
-let textoEncriptado = [];
+
 
 function getText() {
     texto = document.getElementById('texto__encriptable').value;
@@ -27,8 +20,10 @@ function getText() {
    texto =texto.replaceAll('i','imes') ;
    texto =texto.replaceAll('a','ai') ;
    texto =texto.replaceAll('o','ober') ;
-   texto =texto.replaceAll('i','ufat') ;
-console.log(texto);
+   texto =texto.replaceAll('u','ufat') ;
+
+   document.getElementById('copiar_texto').innerHTML= `${texto}`;
+
 
 
 }
@@ -39,6 +34,10 @@ function reverseText(){
     desencriptado = desencriptado.replaceAll('imes','i');
     desencriptado = desencriptado.replaceAll('ai','a');
     desencriptado = desencriptado.replaceAll('ober','o');
-    desencriptado = desencriptado.replaceAll('ufat','i');
-console.log(desencriptado);
+    desencriptado = desencriptado.replaceAll('ufat','u');
+document.getElementById('copiar_texto').innerHTML= `${desencriptado}`;
+}
+
+function copiText(){
+    
 }
